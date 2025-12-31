92b68f1e211efe7f04027c029dfec4726262d264
amirmn8 <porianaghdali@gmail.com>
2025-12-31 13:39:23 +0330

    fix sanity

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,44 +1,30 @@
 import Link from "next/link";
 import { PortableText } from '@portabletext/react'
 
-import { client, urlFor } from "../../../lib/sanity";
-
+import { client,  } from "../../../sanity/lib/client";
+async function getData(slug) {
+  const data = await client.fetch(`*[_type=="post" && slug.current == "${slug}"]{
+    title,
+    slug,body,
+    publishedAt,author,"imageUrl": mainImage.asset->url
+   
+  }`);
+  return data[0];
+}
 export default async function BlogPage({ params }) {
   const { slug } = await params;
  
-  const article = await client.fetch(
-    `*[_type == "article" && slug.current == $slug][0]{
-      title,
-      sections,
-      publishedAt
-    }`,
-    { slug }
-  );
-  console.log(article);
+ const article = await getData(slug);
+ console.log(article, "articles");
   return (
     <div className="px-[5vw] py-[5vw]">
-      <h1 className="text-4xl font-bold mb-4 text-white">{article.title}</h1>
+       <h1 className="text-4xl font-bold mb-4 text-white">{article.title}</h1> 
       <p className="text-white mb-6">{new Date(article.publishedAt).toLocaleDateString()}</p>
 
       <div className="space-y-6 text-white">
-        <PortableText
-          value={article.sections}
-          components={{
-            types: {
-              image: ({ value }) => (
-                <img
-                  src={urlFor(value.asset).width(400).height(400).url()}
-                  alt={article.title}
-                  className="rounded-xl"
-                />
-              ),
-            },
-            marks: {
-              strong: ({ children }) => <strong className="font-bold">{children}</strong>,
-              em: ({ children }) => <em>{children}</em>,
-              // میتونی لینک و بقیه markها هم اضافه کنی
-            },
-          }}
+         <PortableText
+          value={article.body}
+          
         />
       </div>
     </div>
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,39 +1,48 @@
-import Link from "next/link"
-import { client, urlFor } from "../../lib/sanity"
+import Link from "next/link";
+import { client } from "../../sanity/lib/client";
 
-export default async function BlogPage() {
-  const articles = await client.fetch(`*[_type=="article"]{
+async function getData() {
+  const data = await client.fetch(`*[_type=="post"]{
     title,
     slug,
-    publishedAt,
-    coverImage
-  }`)
-  
-  console.log("Articles fetched:", articles)
+    publishedAt,author,"imageUrl": mainImage.asset->url
+   
+  }`);
+  return data;
+}
 
+export const revalidate = 60; 
+
+export default async function BlogPage() {
+  const articles = await getData();
+  console.log(articles, "articles");
   return (
     <div className="px-[5vw] py-[5vw]">
       <h1 className="text-3xl font-bold mb-6 text-white">Blog</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {articles.map((art, index) => {
-          const slug = art.slug?.current || `article-${index}`
-          const imageUrl = art.coverImage
-            ? urlFor(art.coverImage).width(400).height(250).url()
-            : "/fallback.jpg"
+          const slug = art.slug?.current || `article-${index}`;
+          const imageUrl = art.imageUrl || "/fallback.jpg";
 
           return (
             <Link key={slug} href={`/blog/${slug}`}>
               <div className="rounded-xl overflow-hidden bg-white cursor-pointer">
-                <img src={imageUrl} alt={art.title} className="w-full object-cover row-span-2" />
+                <img
+                  src={imageUrl}
+                  alt={art.title}
+                  className="w-full object-cover row-span-2"
+                />
                 <div className="p-4">
                   <h2 className="font-semibold text-lg">{art.title}</h2>
-                  <p className="text-sm">{new Date(art.publishedAt).toLocaleDateString()}</p>
+                  <p className="text-sm">
+                    {new Date(art.publishedAt).toLocaleDateString()}
+                  </p>
                 </div>
               </div>
             </Link>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
